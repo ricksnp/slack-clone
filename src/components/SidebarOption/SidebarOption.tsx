427720f4a366d8d2faf6b62b2e196808a7a4c67f
Nathan Ricks <rickspnathan@gmail.com>
2020-12-29 17:04:28 -0800

    Essentially finished sidebar and connected to Firebase DB

--- src/components/SidebarOption/SidebarOption.tsx
+++ src/components/SidebarOption/SidebarOption.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
-import './SidebarOption.css';
+import { SvgIconTypeMap } from "@material-ui/core";
+import { OverridableComponent } from "@material-ui/core/OverridableComponent";
+import React from "react";
+import "./SidebarOption.css";
 
-function SidebarOption() {
-    return (
-        <div className="sidebarOption">
-            
-        </div>
-    )
+export interface IProps {
+  title: String;
+  Icon?: OverridableComponent<SvgIconTypeMap<{}, "svg">>;
+  id?: String;
 }
 
-export default SidebarOption
+function SidebarOption({ title, Icon }: IProps) {
+  return (
+    <div className="sidebarOption">
+      {Icon && <Icon className="sidebarOption__icon" />}
+      {Icon ? (
+        <h3>{title}</h3>
+      ) : (
+        <h3 className="sidebarOption__channel">
+          <span className="sidebarOption__hash">#</span>
+          {title}
+        </h3>
+      )}
+    </div>
+  );
+}
+
+export default SidebarOption;
